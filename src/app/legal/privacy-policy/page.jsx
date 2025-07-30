2146ce75e66ae77b828d9a95cc526fae1682ecef
valeraviachalo <valeraviachalo@gmail.com>
2025-07-30 16:40:03 +0300

    feat: restore LegalPage rendering in Privacy Policy page

--- src/app/legal/privacy-policy/page.jsx
+++ src/app/legal/privacy-policy/page.jsx
@@ -1,19 +1,18 @@
 import LegalPage from '@/components/LegalPage/LegalPage'
-import { client } from '@/lib/sanity/client'
-import { PRIVACY_POLICY_QUERY } from '@/lib/sanity/queries'
+// import { client } from '@/lib/sanity/client'
+// import { PRIVACY_POLICY_QUERY } from '@/lib/sanity/queries'
 import React from 'react'
 
 import data from '@/app/preparedData/privacyPolicy.json'
 
 export default async function page() {
-  const dataSanity = await client.fetch(PRIVACY_POLICY_QUERY);
+  // const dataSanity = await client.fetch(PRIVACY_POLICY_QUERY);
 
-  console.log("Privacy Policy Data:", dataSanity);
+  // console.log("Privacy Policy Data:", dataSanity);
 
   return (
-    // <LegalPage 
-    //   data={data}
-    // />
-    <h1></h1>
+    <LegalPage 
+      data={data}
+    />
   )
 }
