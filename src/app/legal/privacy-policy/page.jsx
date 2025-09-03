9cdbae7aa770289f1c1b71782499616b37e8846b
valeraviachalo <valeraviachalo@gmail.com>
2025-09-03 15:02:09 +0300

    fix: Remove unused import of privacyPolicy.json in privacy policy page

--- src/app/legal/privacy-policy/page.jsx
+++ src/app/legal/privacy-policy/page.jsx
@@ -3,7 +3,7 @@ import { client } from '@/lib/sanity/client'
 import { PRIVACY_POLICY_QUERY } from '@/lib/sanity/queries'
 import React from 'react'
 
-import data from '@/app/preparedData/privacyPolicy.json'
+export const revalidate = 60;
 
 export default async function page() {
   const dataSanity = await client.fetch(PRIVACY_POLICY_QUERY);
