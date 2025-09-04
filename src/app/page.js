2786da281fb948a90918f3ae823a9f13112d2ba9
valeraviachalo <valeraviachalo@gmail.com>
2025-09-04 13:09:29 +0300

    Placeholder data

--- src/app/page.js
+++ src/app/page.js
@@ -4,15 +4,17 @@ import Contact from "@/utils/Contact/Contact";
 import { HOME_QUERY } from "@/lib/sanity/queries";
 import { client } from "@/lib/sanity/client";
 
+import data from './preparedData/home.json';
+
 export const revalidate = 60;
 
 export default async function Home() {
-  const dataSanity = await client.fetch(HOME_QUERY, {
-    pageId: "homepage"
-  });
+  // const dataSanity = await client.fetch(HOME_QUERY, {
+  //   pageId: "homepage"
+  // });
   
   return (
-    <DataProvider data={dataSanity}>
+    <DataProvider data={data}>
       <HomePage />
       <Contact />
     </DataProvider>
