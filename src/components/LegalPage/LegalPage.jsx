3ad5130782e6b328198facdd36fafbad37cebdf6
valeraviachalo <valeraviachalo@gmail.com>
2025-09-12 12:55:47 +0300

    fix: Add scroll to top effect on Blog, Legal, and Post pages

--- src/components/LegalPage/LegalPage.jsx
+++ src/components/LegalPage/LegalPage.jsx
@@ -1,21 +1,26 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import "./LegalPage.scss";
 import SanityBlockContent from "@sanity/block-content-to-react";
 import BlockContent from "@/utils/BlockContent/BlockContent";
+import { motion } from "framer-motion";
+import { anim, PageAnim } from "@/lib/helpers/anim";
 
 export default function LegalPage({ data }) {
+  useEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, []);
+
   return (
-    <div className="legal-page">
+    <motion.main className="legal-page" {...anim(PageAnim)}>
       <div className="top container">
         <p className="date shadow">{data?.changedDate}</p>
         <h1 className="title">{data?.title}</h1>
       </div>
 
       <div className="content">
-        {data?.blockContent && (
-          <BlockContent content={data.blockContent} />
-        )}
+        {data?.blockContent && <BlockContent content={data.blockContent} />}
       </div>
-    </div>
+    </motion.main>
   );
 }
--- src/components/PostPage/PostPage.jsx
+++ src/components/PostPage/PostPage.jsx
@@ -1,11 +1,15 @@
 "use client"
-import React from 'react'
+import React, { useEffect } from 'react'
 import PostHero from './Hero/PostHero'
 import PostSections from './PostSections/PostSections'
 import { motion } from 'framer-motion'
 import { anim, PageAnim } from '@/lib/helpers/anim'
 
 export default function PostPage() {
+    useEffect(() => {
+      document.documentElement.scrollTo(0, 0);
+    }, []);
+  
   return (
     <motion.main className="post-page" {...anim(PageAnim)}>
       <PostHero />
